f69b566ab5ebe120f8c384d86eb0523e2d110274
m <mmailm@mail.ru>
2015-03-07 23:55:49 +0800

    update tests for manager

--- profiles/tests/test_manager.py
+++ profiles/tests/test_manager.py
@@ -1,3 +1,4 @@
+import json
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from profiles.models import Profile, ProfilePasskeys
@@ -5,7 +6,6 @@ from app.utils import TestCaseEx
 
 
 class TestManagerViews(TestCaseEx):
-
     def test_guest_cant_work_with_manager_views(self):
         profile = Profile.objects.create(name=u"name")
         user = User.objects.first()
@@ -16,26 +16,85 @@ class TestManagerViews(TestCaseEx):
         self.redirect_to_login_on_get("profiles.views.manager.manager")
         self.redirect_to_login_on_post("profiles.views.manager.manager")
 
-        self.redirect_to_login_on_get("profiles.views.manager.get_profile_users", pargs=[profile.pk])
-        self.redirect_to_login_on_post("profiles.views.manager.get_profile_users", pargs=[profile.pk])
-
-        self.redirect_to_login_on_get("profiles.views.manager.get_user_profiles", pargs=[user.pk])
-        self.redirect_to_login_on_post("profiles.views.manager.get_user_profiles", pargs=[user.pk])
-
-        self.redirect_to_login_on_get("profiles.views.manager.add_user_to_profile", pargs=[user.pk, profile.pk])
-        self.redirect_to_login_on_post("profiles.views.manager.add_user_to_profile", pargs=[user.pk, profile.pk])
-
-        self.redirect_to_login_on_get("profiles.views.manager.remove_user_from_profile", pargs=[user.pk, profile.pk])
-        self.redirect_to_login_on_post("profiles.views.manager.remove_user_from_profile", pargs=[user.pk, profile.pk])
-
-        self.redirect_to_login_on_get("profiles.views.manager.update_user_profile_passkey", pargs=[user.pk, profile.pk])
-        self.redirect_to_login_on_post("profiles.views.manager.update_user_profile_passkey", pargs=[user.pk, profile.pk])
+        # self.redirect_to_login_on_get("profiles.views.manager.get_profile_users", pargs=[profile.pk])
+        # self.redirect_to_login_on_post("profiles.views.manager.get_profile_users", pargs=[profile.pk])
+        #
+        # self.redirect_to_login_on_get("profiles.views.manager.get_user_profiles", pargs=[user.pk])
+        # self.redirect_to_login_on_post("profiles.views.manager.get_user_profiles", pargs=[user.pk])
+        #
+        # self.redirect_to_login_on_get("profiles.views.manager.add_user_to_profile", pargs=[user.pk, profile.pk])
+        # self.redirect_to_login_on_post("profiles.views.manager.add_user_to_profile", pargs=[user.pk, profile.pk])
+        #
+        # self.redirect_to_login_on_get("profiles.views.manager.remove_user_from_profile", pargs=[user.pk, profile.pk])
+        # self.redirect_to_login_on_post("profiles.views.manager.remove_user_from_profile", pargs=[user.pk, profile.pk])
+        #
+        # self.redirect_to_login_on_get("profiles.views.manager.update_user_profile_passkey", pargs=[user.pk, profile.pk])
+        # self.redirect_to_login_on_post("profiles.views.manager.update_user_profile_passkey", pargs=[user.pk, profile.pk])
 
         # self.redirect_to_login_on_get("profiles.views.manager.check_passkey", pargs=[profile.pk])
         # self.redirect_to_login_on_post("profiles.views.manager.check_passkey", pargs=[profile.pk])
 
 
-    def test_user_cant_work_with_manager_views(self):
-        pass
+    def test_guest_cant_update_profile_passkeys(self):
+        self.redirect_to_login_on_get("profiles.views.manager.update_profile_passkeys")
+        self.redirect_to_login_on_post("profiles.views.manager.update_profile_passkeys")
 
+    @TestCaseEx.superuser
+    def test_update_profile_passkeys_should_update_existing_passkeys(self):
+        ProfilePasskeys.objects.all().delete()
 
+        profile = Profile.objects.create(name=u"name")
+        user = User.objects.first()
+        passkey = ProfilePasskeys.objects.create(profile=profile, user=user, passkey=u'12345')
+
+        new_values = {
+            "profile_passkeys": json.dumps([
+                {"profile": profile.pk, "user": user.pk, "passkey": u'54321'}
+            ])
+        }
+
+        self.can_post("profiles.views.manager.update_profile_passkeys", params=new_values)
+
+        passkey = ProfilePasskeys.objects.get(pk=passkey.pk)
+
+        self.assertEqual(passkey.passkey, u'54321')
+
+    @TestCaseEx.superuser
+    def test_update_profile_passkeys_can_create_new_passkkey_triple(self):
+        ProfilePasskeys.objects.all().delete()
+
+        profile = Profile.objects.create(name=u"name")
+        user = User.objects.first()
+
+        new_values = {
+            "profile_passkeys": json.dumps([
+                {"profile": profile.pk, "user": user.pk, "passkey": u'54321'}
+            ])
+        }
+
+        self.can_post("profiles.views.manager.update_profile_passkeys", params=new_values)
+
+        passkey = ProfilePasskeys.objects.order_by("-pk").first()
+
+        self.assertEqual(passkey.passkey, u'54321')
+
+    @TestCaseEx.superuser
+    def test_update_profile_passkeys_can_delete_existing_triples(self):
+        ProfilePasskeys.objects.all().delete()
+
+        profile = Profile.objects.create(name=u"name")
+        profile2 = Profile.objects.create(name=u"name2")
+        user = User.objects.first()
+        passkey = ProfilePasskeys.objects.create(profile=profile, user=user, passkey=u'12345')
+        passkey = ProfilePasskeys.objects.create(profile=profile2, user=user, passkey=u'12345')
+
+        new_values = {
+            "profile_passkeys": json.dumps([
+                {"profile": profile.pk, "user": user.pk, "passkey": u'', u'allowed': 'false'}
+            ])
+        }
+
+        self.can_post("profiles.views.manager.update_profile_passkeys", params=new_values)
+
+        self.assertEqual(ProfilePasskeys.objects.filter(profile=profile2).count(), 1)
+        self.assertEqual(ProfilePasskeys.objects.filter(profile=profile).count(), 0)
--- profiles/views/manager.py
+++ profiles/views/manager.py
@@ -1,9 +1,13 @@
+import json
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.core.serializers import json, serialize
+from django.core.serializers import serialize
 from django.core.urlresolvers import reverse
+from django.db.transaction import atomic
 from django.http.response import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
+from app.utils import require_in_POST
 
 from profiles.forms import ProfileForm, PasskeyForm, ProfilePasskeysForm
 from profiles.models import Profile, ProfilePasskeys
@@ -19,79 +23,108 @@ def manager(request):
 
 
 @login_required
-def get_profile_users(request, profile_id):
+@require_POST
+@require_in_POST('profile_passkeys')
+@atomic
+def update_profile_passkeys(request):
     """
-    :return: list of users which can work with profile with profile_id
+    this methods expects POST method,
+    expects the list of triples in profile_passkeys parameter
+    (profile, user, passkey)
     """
-    data = User.objects.filter(pk__in=ProfilePasskeys.objects.filter(profile=profile_id).values("user"))
-    data = serialize('json', data)
-    return HttpResponse(data, content_type='json')
+    profiles_passkeys = json.loads(request.POST['profile_passkeys'])
+    for profile_passkey in profiles_passkeys:
+        profile_id = profile_passkey['profile']
+        user_id = profile_passkey['user']
 
+        if 'allowed' in profile_passkey:
+            if profile_passkey['allowed'] == u'false':
+                ppk = ProfilePasskeys.objects.filter(profile_id=profile_id, user_id=user_id)
+                ppk.delete()
+                continue
 
-@login_required
-def get_user_profiles(request, user_id):
-    """
-    :return: list of profiles with which user can work
-    """
-    user = get_object_or_404(User, pk=user_id)
-    data = serialize('json', ProfilePasskeys.objects.filter(user=user))
-    return HttpResponse(data, content_type='json')
-
-
-@login_required
-def add_user_to_profile(request, user_id, profile_id):
-    """
-    add to user ability to work with profile
-    """
-    profile = get_object_or_404(Profile, pk=profile_id)
-    if request.method == "GET":
-        form = PasskeyForm()
-        return render(request, "profiles/manager/add_user_to_profile.html", {
-            'form': form
-        })
-    elif request.method == "POST":
-        form = PasskeyForm(request.POST)
-        if form.is_valid():
-            ppk = ProfilePasskeys.objects.get_or_create(profile_id=profile_id, user_id=user_id)
-            ppk.passkey = form.cleande_data['passkey']
-            ppk.save()
-            return HttpResponse()
-        elif request.is_ajax():
-            return HttpResponseBadRequest()
-        else:
-            return render(request, "profiles/manager/add_user_to_profile.html", {
-                'form': form
-            })
-    return HttpResponseBadRequest()
-
-
-@login_required
-def remove_user_from_profile(request, user_id, profile_id):
-    passkey_obj = get_object_or_404(ProfilePasskeys, profile_id=profile_id, user_id=user_id)
-    passkey_obj.delete()
+        passkey = profile_passkey['passkey']
+        ppk, _ = ProfilePasskeys.objects.get_or_create(profile_id=profile_id, user_id=user_id)
+        ppk.passkey = passkey
+        ppk.save()
     return HttpResponse()
 
 
-@login_required
-def update_user_profile_passkey(request, user_id, profile_id):
-    passkey_obj = get_object_or_404(ProfilePasskeys, profile_id=profile_id, user_id=user_id)
-    if request.method == "GET":
-        form = ProfilePasskeysForm()
-        return render(request, "profiles/manager/update_user_profile_passkey.html", {
-            'form': form
-        })
-    elif request.method == "POST":
-        form = ProfilePasskeysForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponse()
-        elif request.is_ajax():
-            return HttpResponseBadRequest()
-        else:
-            return render(request, "profiles/manager/update_user_profile_passkey.html", {
-                'form': form
-            })
-    return HttpResponseBadRequest()
+#
+# @login_required
+# def get_profile_users(request, profile_id):
+# """
+#     :return: list of users which can work with profile with profile_id
+#     """
+#     data = User.objects.filter(pk__in=ProfilePasskeys.objects.filter(profile=profile_id).values("user"))
+#     data = serialize('json', data)
+#     return HttpResponse(data, content_type='json')
+#
+#
+# @login_required
+# def get_user_profiles(request, user_id):
+#     """
+#     :return: list of profiles with which user can work
+#     """
+#     user = get_object_or_404(User, pk=user_id)
+#     data = serialize('json', ProfilePasskeys.objects.filter(user=user))
+#     return HttpResponse(data, content_type='json')
+#
+#
+# @login_required
+# def add_user_to_profile(request, user_id, profile_id):
+#     """
+#     add to user ability to work with profile
+#     """
+#     profile = get_object_or_404(Profile, pk=profile_id)
+#     if request.method == "GET":
+#         form = PasskeyForm()
+#         return render(request, "profiles/manager/add_user_to_profile.html", {
+#             'form': form
+#         })
+#     elif request.method == "POST":
+#         form = PasskeyForm(request.POST)
+#         if form.is_valid():
+#             ppk = ProfilePasskeys.objects.get_or_create(profile_id=profile_id, user_id=user_id)
+#             ppk.passkey = form.cleande_data['passkey']
+#             ppk.save()
+#             return HttpResponse()
+#         elif request.is_ajax():
+#             return HttpResponseBadRequest()
+#         else:
+#             return render(request, "profiles/manager/add_user_to_profile.html", {
+#                 'form': form
+#             })
+#     return HttpResponseBadRequest()
+#
+#
+# @login_required
+# def remove_user_from_profile(request, user_id, profile_id):
+#     passkey_obj = get_object_or_404(ProfilePasskeys, profile_id=profile_id, user_id=user_id)
+#     passkey_obj.delete()
+#     return HttpResponse()
+#
+#
+# @login_required
+# def update_user_profile_passkey(request, user_id, profile_id):
+#     passkey_obj = get_object_or_404(ProfilePasskeys, profile_id=profile_id, user_id=user_id)
+#     if request.method == "GET":
+#         form = ProfilePasskeysForm()
+#         return render(request, "profiles/manager/update_user_profile_passkey.html", {
+#             'form': form
+#         })
+#     elif request.method == "POST":
+#         form = ProfilePasskeysForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse()
+#         elif request.is_ajax():
+#             return HttpResponseBadRequest()
+#         else:
+#             return render(request, "profiles/manager/update_user_profile_passkey.html", {
+#                 'form': form
+#             })
+#     return HttpResponseBadRequest()
 
 
 @login_required
